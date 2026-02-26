af16faa58f7199f590b477b682561d1e75e77d4f
PCloud <heige.pcloud@outlook.com>
2026-02-26 18:56:29 +0000

    Refactor AvailableExpression namespace and update references in Example.lean for consistency

--- ProgramAnalysis/DataFlowAnalysis/Analysis.lean
+++ ProgramAnalysis/DataFlowAnalysis/Analysis.lean
@@ -8,31 +8,32 @@ public import ProgramAnalysis.DataFlowAnalysis.Equation
 
 namespace ProgramAnalysis.DataFlowAnalysis
 
-namespace AvailableExpression
 open While
 
-def AE.kill (stmt : Stmt) : Block → List AExp
+namespace AvailableExpression
+
+def kill (stmt : Stmt) : Block → List AExp
   | .assign x _ _ => stmt.aexp.filter (fun a' => a'.FV.elem x)
   | _ => ∅
 
-def AE.gen (_ : Stmt) : Block → List AExp
+def gen (_ : Stmt) : Block → List AExp
   | .assign x a _ => a.aexp.filter (fun a' => !(a'.FV.elem x))
   | _ => ∅
 
-def AE.entry (s : Stmt) (l : Label) : Equation AExp :=
+def entry (s : Stmt) (l : Label) : Equation AExp :=
   let lhs := Equation.Atom.mk l .e0
   if l = s.init then ⟨lhs, .empty⟩
   else ⟨lhs, inters ((s.flow.filter (fun (_, ll) => l == ll)).map (fun (l', _l) => .var (Equation.Atom.mk l' .e1)))⟩
 
-def AE.exit (s : Stmt) (l : Label) : Equation AExp :=
+def exit (s : Stmt) (l : Label) : Equation AExp :=
   let lhs := Equation.Atom.mk l .e1
   let b := s.block! l
   ⟨lhs, .union (.diff (.var ⟨l, .e0⟩) (.lit (.ofList (kill s b)))) (.lit (.ofList (gen s b)))⟩
 
-public def AE.equations (s : Stmt) : List (Equation AExp) :=
-  s.labels.flatMap (fun l => [AE.entry s l, AE.exit s l])
+public def equations (s : Stmt) : List (Equation AExp) :=
+  s.labels.flatMap (fun l => [entry s l, exit s l])
 
-public def AE.init [Ord α] (es : List (Equation α))
+public def init [Ord α] (es : List (Equation α))
   : Std.TreeMap Equation.Atom (Std.TreeSet α) :=
   es.foldl (fun acc eq => acc.insert eq.lhs .empty) .empty
 
--- ProgramAnalysis/DataFlowAnalysis/Example.lean
+++ ProgramAnalysis/DataFlowAnalysis/Example.lean
@@ -4,7 +4,9 @@ import ProgramAnalysis.DataFlowAnalysis
 
 namespace ProgramAnalysis.DataFlowAnalysis.Example
 
-open ProgramAnalysis.DataFlowAnalysis While AvailableExpression
+open ProgramAnalysis.DataFlowAnalysis While
+
+section AE
 
 def example1 : Stmt := [While|
   x := a + b;
@@ -17,11 +19,16 @@ def example1 : Stmt := [While|
 
 #eval example1
 
-def equations := AE.equations example1
+def equations := AvailableExpression.equations example1
+
 #eval equations.forM (fun eq => IO.println eq.toString)
 
-def solution := chaoticIteration equations AE.init
-#eval solution.toList.forM 
+def solution := chaoticIteration equations AvailableExpression.init
+
+#eval solution.toList.forM
   (fun (k, v) => IO.println s!"{k} = {v.toList.map (fun a: AExp => a.toString)}")
+
+end AE
+
 
 end ProgramAnalysis.DataFlowAnalysis.Example
