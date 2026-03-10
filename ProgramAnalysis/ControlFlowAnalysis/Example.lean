7c99446a10fc2b224a28d9ee60764d9b71567798
PCloud <heige.pcloud@outlook.com>
2026-03-10 13:25:10 +0000

    Add detailed info comments and guard messages for example evaluations in Example.lean

--- ProgramAnalysis/ControlFlowAnalysis/Example.lean
+++ ProgramAnalysis/ControlFlowAnalysis/Example.lean
@@ -31,16 +31,61 @@ def expr4 : Expr := [Fun|
 
 def expr := expr4
 
-#eval IO.println expr.toString
+/--
+info: (let g = (fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶)⁷ in (g⁸ (fn y => (y⁹ > 2¹⁰)¹¹)¹²)¹³)¹⁴
+-/
+#guard_msgs in
+#eval IO.println expr
 
-#eval Value.toString <$> (expr.eval .empty)
+/-- info: (some 10) -/
+#guard_msgs in
+#eval IO.println (expr.eval .empty)
 
 def example.constraint := expr.constraints.run expr.allFns
 
-#eval example.constraint.toList.forM (fun c => IO.println c)
+/--
+info: C(4) ⊆ C(6)
+C(5) ⊆ C(6)
+C(7) ⊆ r(g)
+C(13) ⊆ C(14)
+r(f) ⊆ C(1)
+r(g) ⊆ C(8)
+r(y) ⊆ C(9)
+fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶ ⊆ C(7)
+fn y => (y⁹ > 2¹⁰)¹¹ ⊆ C(12)
+fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶ ⊆ C(1) => C(2) ⊆ r(f)
+fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶ ⊆ C(1) => C(6) ⊆ C(3)
+fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶ ⊆ C(8) => C(6) ⊆ C(13)
+fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶ ⊆ C(8) => C(12) ⊆ r(f)
+fn y => (y⁹ > 2¹⁰)¹¹ ⊆ C(1) => C(2) ⊆ r(y)
+fn y => (y⁹ > 2¹⁰)¹¹ ⊆ C(1) => C(11) ⊆ C(3)
+fn y => (y⁹ > 2¹⁰)¹¹ ⊆ C(8) => C(11) ⊆ C(13)
+fn y => (y⁹ > 2¹⁰)¹¹ ⊆ C(8) => C(12) ⊆ r(y)
+-/
+#guard_msgs in
+#eval example.constraint.toList.forM IO.println
 
 def example.solution := Constraint.solve example.constraint.toList
 
+/--
+info: C(1) ↦ [fn y => (y⁹ > 2¹⁰)¹¹]
+C(2) ↦ []
+C(3) ↦ []
+C(4) ↦ []
+C(5) ↦ []
+C(6) ↦ []
+C(7) ↦ [fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶]
+C(8) ↦ [fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶]
+C(9) ↦ []
+C(11) ↦ []
+C(12) ↦ [fn y => (y⁹ > 2¹⁰)¹¹]
+C(13) ↦ []
+C(14) ↦ []
+r(f) ↦ [fn y => (y⁹ > 2¹⁰)¹¹]
+r(g) ↦ [fn f => (if (f¹ 3²)³ then 10⁴ else 5⁵)⁶]
+r(y) ↦ []
+-/
+#guard_msgs in
 #eval example.solution.toList.forM (fun (node, value) => IO.println s!"{node} ↦ {value.toList.map (fun t: FnTerm => t)}")
 
 end ProgramAnalysis.ControlFlowAnalysis.Example
